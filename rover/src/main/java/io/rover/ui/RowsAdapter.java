4dfdcf025d76e80fd22243b79169ac95956a2b58
ata_n <ata.namvari@gmail.com>
2016-07-04 12:42:07 -0400

    make separate view holders for each block type

--- rover/src/main/java/io/rover/ui/RowsAdapter.java
+++ rover/src/main/java/io/rover/ui/RowsAdapter.java
@@ -1,5 +1,6 @@
 package io.rover.ui;
 
+import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,12 +11,14 @@ import java.util.List;
 
 import io.rover.R;
 import io.rover.model.Block;
+import io.rover.model.ImageBlock;
 import io.rover.model.Row;
+import io.rover.model.TextBlock;
 
 /**
  * Created by ata_n on 2016-06-28.
  */
-public class RowsAdapter extends RecyclerView.Adapter<RowsAdapter.ViewHolder> implements BlockLayoutManager.BlockProvider {
+public class RowsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements BlockLayoutManager.BlockProvider {
 
     private List<Row> mRows;
     private float screenDensity;
@@ -26,15 +29,60 @@ public class RowsAdapter extends RecyclerView.Adapter<RowsAdapter.ViewHolder> im
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View blockView = inflater.inflate(R.layout.text_block, parent, false);
-        ViewHolder viewHolder = new ViewHolder(blockView);
-        return viewHolder;
+    public int getItemViewType(int position) {
+        Block block = getBlockAtPosition(position);
+        if (block instanceof ImageBlock) {
+            return 0;
+        } else if (block instanceof TextBlock) {
+            return 1;
+        }
+        return super.getItemViewType(position);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case 0: return new ImageViewHolder(parent.getContext());
+            case 1: return new TextViewHolder(parent.getContext());
+            default: return new ViewHolder(parent.getContext());
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Block block = getBlockAtPosition(position);
+
+        if (block == null) { return; }
+
+        // Appearance
+
+        GradientDrawable borderDrawable = new GradientDrawable();
+        borderDrawable.setColor(block.getBackgroundColor());
+        borderDrawable.setStroke((int)(block.getBorderWidth() * screenDensity), block.getBorderColor());
+        borderDrawable.setCornerRadius((float)(block.getBorderRadius() * screenDensity));
+        holder.itemView.setBackground(borderDrawable);
+    }
+
+    @Override
+    public int getItemCount() {
+        int numBlocks = 0;
+        for (Row row : mRows) {
+            numBlocks += row.getBlocks().size();
+        }
+        return numBlocks;
+    }
+
+    @Override
+    public int getRowCount() {
+        return mRows.size();
+    }
+
+    @Override
+    public Row getRow(int index) {
+        return mRows.get(index);
+    }
+
+    private Block getBlockAtPosition(int position) {
         Block block = null;
 
         int count = 0;
@@ -48,41 +96,22 @@ public class RowsAdapter extends RecyclerView.Adapter<RowsAdapter.ViewHolder> im
             count += blocks.size();
         }
 
-        if (block == null) { return; }
-
-        GradientDrawable borderDrawable = new GradientDrawable();
-        borderDrawable.setColor(block.getBackgroundColor());
-        borderDrawable.setStroke((int)(block.getBorderWidth() * screenDensity), block.getBorderColor());
-        borderDrawable.setCornerRadius((float)(block.getBorderRadius() * screenDensity));
-        holder.itemView.setBackground(borderDrawable);
-
-
-
-    }
-
-    @Override
-    public int getItemCount() {
-        int numBlocks = 0;
-        for (Row row : mRows) {
-            numBlocks += row.getBlocks().size();
-        }
-        return numBlocks;
+        return block;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-
-        public ViewHolder(View itemView) {
-            super(itemView);
+        public ViewHolder(Context context) {
+            super(new View(context));
         }
     }
 
-    @Override
-    public int getRowCount() {
-        return mRows.size();
+    public static class ImageViewHolder extends RecyclerView.ViewHolder {
+        public ImageViewHolder(Context context) { super(new View(context)); }
     }
 
-    @Override
-    public Row getRow(int index) {
-        return mRows.get(index);
+    public static class TextViewHolder extends RecyclerView.ViewHolder {
+        public TextViewHolder(Context context) { super(new View(context)); }
     }
+
+
 }

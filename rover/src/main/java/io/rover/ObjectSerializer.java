03a1b8d4364a1ac1091572c1db61d36cfd128afa
Chris Recalis <crecalis@gmail.com>
2016-11-01 15:24:21 -0400

    remove unused imports

--- rover/src/main/java/io/rover/ObjectSerializer.java
+++ rover/src/main/java/io/rover/ObjectSerializer.java
@@ -10,9 +10,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Locale;
 
 import io.rover.model.Action;

5f291288c67d39341b492a95da3b384a1283659f
Chris Recalis <crecalis@gmail.com>
2017-01-26 10:18:34 -0500

    bump version

--- rover/src/main/java/io/rover/Rover.java
+++ rover/src/main/java/io/rover/Rover.java
@@ -69,7 +69,7 @@ import io.rover.model.Place;
  */
 public class Rover implements EventSubmitTask.Callback {
 
-    protected static String VERSION = "1.6.0";
+    protected static String VERSION = "1.7.0";
     protected static Rover mSharedInstance = new Rover();
     protected static int NOTIFICATION_ID = 12345;
 

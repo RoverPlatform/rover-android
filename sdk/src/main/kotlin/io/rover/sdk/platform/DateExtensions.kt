1f324e58cad68c179977c0c9e214d3fd44d400e6
Sam Woodall <samwoodall01@gmail.com>
2019-05-10 15:40:45 -0400

    Change date extension to internal visibility

--- sdk/src/main/kotlin/io/rover/sdk/platform/DateExtensions.kt
+++ sdk/src/main/kotlin/io/rover/sdk/platform/DateExtensions.kt
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-fun Date.dateAsIso8601(): String {
+internal fun Date.dateAsIso8601(): String {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.US).format(this)
     } else {

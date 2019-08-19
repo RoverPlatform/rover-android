70e9c2d6df8b2a35f7d106a04fa19de611e7a3d9
Sam Woodall <samwoodall01@gmail.com>
2019-08-19 13:12:11 -0400

    Copy campaigns make-release.sh to make both consistent

--- make-release.sh
+++ make-release.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Sets you up to make a release.  Pass in release name, no prefixed `v`.
+# Sets you up to make a hotfix.  Pass in release name, no prefixed `v`.
 
-if [ -z $1 ]
+export RELEASE_OR_HOTFIX=$1
+VERSION=$2
+
+if [[ $RELEASE_OR_HOTFIX == "release" || ($RELEASE_OR_HOTFIX == "hotfix") ]]
 then
-    echo "Pass in release name, no prefixed 'v'."
+    echo "You have selected $RELEASE_OR_HOTFIX"
+else
+    echo "usage: You must specify one of release or hotfix. eg $0 release 3.0.0"
+    exit -1
+fi
+
+if [ -z $VERSION ]
+then
+    echo "usage: Pass in hotfix name, no prefixed 'v'."
     exit -1
 fi
 
@@ -32,25 +43,33 @@ git checkout develop
 # destroy develop state!
 git reset --hard origin/develop
 
-echo "Verifying SDK."
-DEPLOY_MAVEN_PATH=`pwd`/maven ./gradlew clean test
+echo "Making $RELEASE_OR_HOTFIX branch for: $VERSION"
 
-echo "Making release branch for: $1"
+if [ $RELEASE_OR_HOTFIX == "hotfix" ]
+then
+  echo "Assuming hotfix branch already exists."
+  git checkout hotfix/$VERSION
+  echo "Verifying SDK."
+  DEPLOY_MAVEN_PATH=`pwd`/maven ./gradlew clean test
+else
+  git checkout develop
+  echo "Verifying SDK."
+  DEPLOY_MAVEN_PATH=`pwd`/maven ./gradlew clean test
+  git flow $RELEASE_OR_HOTFIX start $VERSION
+fi
 
-git flow release start $1
-
-echo "Edit your version numbers (build.gradle and README.md) and press return!"
+echo "Edit your version numbers (README.md and build.gradle) and press return!"
 read -n 1
 
 echo "Building SDK."
-DEPLOY_MAVEN_PATH=`pwd`/maven ./gradlew clean test sdk:assembleRelease sdk:publishProductionPublicationToMavenRepository
+DEPLOY_MAVEN_PATH=`pwd`/maven time ./gradlew clean test :core:assembleRelease :core:publishProductionPublicationToMavenRepository notifications:assembleRelease notifications:publishProductionPublicationToMavenRepository location:assembleRelease location:publishProductionPublicationToMavenRepository debug:assembleRelease debug:publishProductionPublicationToMavenRepository ticketmaster:assembleRelease ticketmaster:publishProductionPublicationToMavenRepository advertising:assembleRelease advertising:publishProductionPublicationToMavenRepository experiences:assembleRelease experiences:publishProductionPublicationToMavenRepository --info --stacktrace
 
 # add the untracked files that have appeared under maven with the new bits.
 git add maven
 
-git commit -a -m "Releasing $1."
+git commit -a -m "Releasing $VERSION."
 
-git flow release finish $1
+git flow $RELEASE_OR_HOTFIX finish $VERSION
 
 # move the gh-pages branch to develop to keep hosting (and allowing any pre-release builds in develop to be exposed for download).
 git branch -f gh-pages develop
@@ -58,3 +77,4 @@ git branch -f gh-pages develop
 git push origin master
 git push origin develop
 git push origin gh-pages
+git push origin v$VERSION
